ba05e053f6307267346b375f3d7038b7d8156e68
Oleksandr Masiuk <ommasiuk@gmail.com>
2020-01-27 10:00:42 +0200

    third variant

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 "use strict";
-// SIZE_SMALL = 50;
-// SIZE_MEDIUM = 75;
-// SIZE_LARGE = 100;
-// TOPPING_MAYO = //добавка из майонеза
-// TOPPING_SAUCE = //еще приправу
-// let addThing = [];
 class Hamburger {
     static SIZE_SMALL = {
         price: 50,
@@ -18,53 +12,44 @@ class Hamburger {
         price: 15,
         calories: 3
     };
-    constructor(price, calories) {
-        this.calories = calories;
-        this.price = price;
-        this.burger = [];
-        
+    constructor(burger) {
+        this.burger = [burger];
     }
     add(burger) {
          return this.burger.push(burger);
     }
+    calculatePrice(){
+        let sumPrice = 0;
+        for (let currentPrice of this.burger) {
+            const {price} = currentPrice;
+            sumPrice = sumPrice + price;
+         }
+         return sumPrice;
+    }
+    calculateCalories(){
+        let sumCalories = 0;
+        for (let currentCalories of this.burger) {
+            const {calories} = currentCalories;
+            sumCalories = sumCalories + calories;
+         }
+         return sumCalories;
+    }
 }
-
-// Hamburger.prototype.add = function(price) { 
-//    let totalPrice = [this.price].concat([price])
-//     .map((item) => 0 + item.price)
-//     .reduce((acc, el) => acc + el);
-//     console.log('totalPrice', totalPrice);
-//     return totalPrice;
-// }
-
-
-// Hamburger.prototype.addCalories = function(calories) { 
-//     let totalCalories = [this.price].concat([calories])
-//       .map((item) => 0 + item.calories)
-//       .reduce((acc, el) => acc + el);
-//       console.log('totalCalories', totalCalories);
-//       return totalCalories;
-//   }
-
-// Hamburger.prototype.calculatePrice = function(totalPrice, totalCalories) {
-//     console.log('totalPrice', totalPrice);
-//     console.log('totalCalories', totalCalories);
-    
-//    price = {price: totalPrice, calories: totalCalories}
-//     console.log('price', price);
-//     console.log('this.price', this.price);
-//     return totalPrice;
-// }
 
 const hamburger = new Hamburger(Hamburger.SIZE_SMALL);
 console.log(hamburger);
+
 hamburger.add(Hamburger.TOPPING_MAYO);
 console.log(hamburger);
 
+console.log("Total calories: " + hamburger.calculateCalories());
+console.log("Price: " + hamburger.calculatePrice());
+
 hamburger.add(Hamburger.TOPPING_SAUCE);
 console.log(hamburger);
+hamburger.calculatePrice();
 
-// console.log("Price: " + hamburger.calculatePrice());
-
+console.log("Price with sause: " + hamburger.calculatePrice());
+console.log("Total calories with sause: " + hamburger.calculateCalories());
 
 
